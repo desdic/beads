8cfc8eefe095090c4c752adc1dbee8f75978c9e2
kgn <kgn@one.com>
2015-07-30 08:22:54 +0200

    Added a license

--- beads.py
+++ beads.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python
 
-/*
- * ----------------------------------------------------------------------------
- * "THE BEER-WARE LICENSE" (Revision 42):
- * <kn@insecurity.dk> wrote this file.  As long as you retain this notice you
- * can do whatever you want with this stuff. If we meet some day, and you think
- * this stuff is worth it, you can buy me a beer in return.   Kim Nielsen
- * ----------------------------------------------------------------------------
- */
-
+# ----------------------------------------------------------------------------
+# "THE BEER-WARE LICENSE" (Revision 42):
+# <kn@insecurity.dk> wrote this file.  As long as you retain this notice you
+# can do whatever you want with this stuff. If we meet some day, and you think
+# this stuff is worth it, you can buy me a beer in return.   Kim Nielsen
+# ----------------------------------------------------------------------------
 
 import sys, time, getopt
 from PIL import Image, ImageFilter, ImageDraw
